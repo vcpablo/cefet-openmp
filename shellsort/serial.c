3cc764d51cb2548a9f0ce4cd89df8f5e8336ac30
Renato Cardoso Pereira <renatopereira@Renatos-MacBook-Pro.local>
2017-06-28 21:21:32 -0300

    Comments

--- shellsort/serial.c
+++ shellsort/serial.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include <time.h>
 #include "../utils.h"
-#define ARRAYSIZE 1000000
+#define ARRAYSIZE 100
 
 void shellsort( int *vet, int size ) {
     int i, j, value;
     int gap = 1;
     
+    // Criando o gap para divisão e comparação do sort
     while( gap < size ) {
+        // Cada incremento não pode ser múltiplo do anterior, essa fórmula é a de melhor performance
         gap = 3 * gap + 1;
     }
     
     while ( gap > 1 ) {
+        // Antes de iniciar a iteração o gap é dividido, para poder comparar objetos mais próximos de acordo com as iterações
         gap /= 3;
 
+        // I inicia a partir do gap
         for( i = gap; i < size; i++ ) {
-            value = vet[i];
+            value = vet[ i ];
             j = i - gap;
 
             while ( j >= 0 && value < vet[ j ] ) {
